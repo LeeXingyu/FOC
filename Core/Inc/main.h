d48d8ea0624fe57af1b0b5416d0f3c6b4a12e51e
厉鑫 <1931654476@qq.com>
2026-05-13 16:24:29 +0800

    First

---
+++ Core/Inc/main.h
@@ -0,0 +1,149 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32g4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define DRV_INLC_Pin GPIO_PIN_13
+#define DRV_INLC_GPIO_Port GPIOC
+#define MCU_DO1_Pin GPIO_PIN_14
+#define MCU_DO1_GPIO_Port GPIOC
+#define LE0_Pin GPIO_PIN_15
+#define LE0_GPIO_Port GPIOC
+#define COMM_IO2_Pin GPIO_PIN_0
+#define COMM_IO2_GPIO_Port GPIOC
+#define CODER_CS_N2_Pin GPIO_PIN_1
+#define CODER_CS_N2_GPIO_Port GPIOC
+#define VSENB_Pin GPIO_PIN_2
+#define VSENB_GPIO_Port GPIOC
+#define VSENC_Pin GPIO_PIN_3
+#define VSENC_GPIO_Port GPIOC
+#define ISENC_Pin GPIO_PIN_0
+#define ISENC_GPIO_Port GPIOA
+#define ISENB_Pin GPIO_PIN_1
+#define ISENB_GPIO_Port GPIOA
+#define ISENA_Pin GPIO_PIN_2
+#define ISENA_GPIO_Port GPIOA
+#define COMM_ID_Pin GPIO_PIN_4
+#define COMM_ID_GPIO_Port GPIOA
+#define DRV_SCLK_Pin GPIO_PIN_5
+#define DRV_SCLK_GPIO_Port GPIOA
+#define DRV_MISO_Pin GPIO_PIN_6
+#define DRV_MISO_GPIO_Port GPIOA
+#define DRV_MOSI_Pin GPIO_PIN_7
+#define DRV_MOSI_GPIO_Port GPIOA
+#define TSENB_Pin GPIO_PIN_4
+#define TSENB_GPIO_Port GPIOC
+#define TSENC_Pin GPIO_PIN_5
+#define TSENC_GPIO_Port GPIOC
+#define DRV_INLB_Pin GPIO_PIN_0
+#define DRV_INLB_GPIO_Port GPIOB
+#define VSENVM_Pin GPIO_PIN_1
+#define VSENVM_GPIO_Port GPIOB
+#define TSENA_Pin GPIO_PIN_2
+#define TSENA_GPIO_Port GPIOB
+#define COMM_RST_Pin GPIO_PIN_10
+#define COMM_RST_GPIO_Port GPIOB
+#define VSENA_Pin GPIO_PIN_11
+#define VSENA_GPIO_Port GPIOB
+#define COMM_CS_N_Pin GPIO_PIN_12
+#define COMM_CS_N_GPIO_Port GPIOB
+#define COMM_SCK_Pin GPIO_PIN_13
+#define COMM_SCK_GPIO_Port GPIOB
+#define COMM_MISO_Pin GPIO_PIN_14
+#define COMM_MISO_GPIO_Port GPIOB
+#define COMM_MOSI_Pin GPIO_PIN_15
+#define COMM_MOSI_GPIO_Port GPIOB
+#define BRAKE_PWM_Pin GPIO_PIN_9
+#define BRAKE_PWM_GPIO_Port GPIOC
+#define DRV_NHC_Pin GPIO_PIN_8
+#define DRV_NHC_GPIO_Port GPIOA
+#define DRV_INHB_Pin GPIO_PIN_9
+#define DRV_INHB_GPIO_Port GPIOA
+#define DRV_INHA_Pin GPIO_PIN_10
+#define DRV_INHA_GPIO_Port GPIOA
+#define COMM_IO1_Pin GPIO_PIN_15
+#define COMM_IO1_GPIO_Port GPIOA
+#define COMM_INT_Pin GPIO_PIN_10
+#define COMM_INT_GPIO_Port GPIOC
+#define MCU_DI1_Pin GPIO_PIN_11
+#define MCU_DI1_GPIO_Port GPIOC
+#define CODER_CS_N1_Pin GPIO_PIN_12
+#define CODER_CS_N1_GPIO_Port GPIOC
+#define DRV_CS_N_Pin GPIO_PIN_2
+#define DRV_CS_N_GPIO_Port GPIOD
+#define CODER_SCK_Pin GPIO_PIN_3
+#define CODER_SCK_GPIO_Port GPIOB
+#define CODER_MISO_Pin GPIO_PIN_4
+#define CODER_MISO_GPIO_Port GPIOB
+#define CODER_MOSI_Pin GPIO_PIN_5
+#define CODER_MOSI_GPIO_Port GPIOB
+#define DRV_EN_Pin GPIO_PIN_6
+#define DRV_EN_GPIO_Port GPIOB
+#define DRV_FAULT_N_Pin GPIO_PIN_7
+#define DRV_FAULT_N_GPIO_Port GPIOB
+#define DRV_INLA_Pin GPIO_PIN_9
+#define DRV_INLA_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ Core/Src/gpio.c
@@ -0,0 +1,132 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    gpio.c
+  * @brief   This file provides code for the configuration
+  *          of all used GPIO pins.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gpio.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/*----------------------------------------------------------------------------*/
+/* Configure GPIO                                                             */
+/*----------------------------------------------------------------------------*/
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/** Configure pins as
+        * Analog
+        * Input
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, MCU_DO1_Pin|LE0_Pin|CODER_CS_N2_Pin|CODER_CS_N1_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, COMM_RST_Pin|DRV_EN_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(DRV_CS_N_GPIO_Port, DRV_CS_N_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : MCU_DO1_Pin LE0_Pin */
+  GPIO_InitStruct.Pin = MCU_DO1_Pin|LE0_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : COMM_IO2_Pin */
+  GPIO_InitStruct.Pin = COMM_IO2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(COMM_IO2_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : CODER_CS_N2_Pin CODER_CS_N1_Pin */
+  GPIO_InitStruct.Pin = CODER_CS_N2_Pin|CODER_CS_N1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : COMM_RST_Pin */
+  GPIO_InitStruct.Pin = COMM_RST_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(COMM_RST_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : COMM_IO1_Pin */
+  GPIO_InitStruct.Pin = COMM_IO1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(COMM_IO1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : COMM_INT_Pin */
+  GPIO_InitStruct.Pin = COMM_INT_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(COMM_INT_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : MCU_DI1_Pin */
+  GPIO_InitStruct.Pin = MCU_DI1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(MCU_DI1_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : DRV_CS_N_Pin */
+  GPIO_InitStruct.Pin = DRV_CS_N_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(DRV_CS_N_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : DRV_EN_Pin */
+  GPIO_InitStruct.Pin = DRV_EN_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(DRV_EN_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : DRV_FAULT_N_Pin */
+  GPIO_InitStruct.Pin = DRV_FAULT_N_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_FALLING;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(DRV_FAULT_N_GPIO_Port, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 2 */
+
+/* USER CODE END 2 */
